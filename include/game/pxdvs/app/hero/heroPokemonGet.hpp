6fff7bb7509c8b7dc2244522a0dc727dacb55e78
HidoranBlaze <blastdroid321@gmail.com>
2025-12-20 03:01:32 +0900

    add fields to HeroPokemonGetParam

--- include/game/pxdvs/app/hero/heroPokemonGet.hpp
+++ include/game/pxdvs/app/hero/heroPokemonGet.hpp
@@ -1,6 +1,23 @@
 #ifndef HERO_POKEMON_GET_H
 #define HERO_POKEMON_GET_H
 
-struct HeroPokemonGetParam {};
+#include <global.h>
+
+struct HeroPokemonGetParam {
+  u8 trainerSexID;         // 0x0
+  u8 catchLevel;           // 0x1
+  u16 pokemonDataID;       // 0x2
+  u16 shadowPokemonDataID; // 0x4
+  u16 friendLevel;         // 0x6
+  u16 move1;               // 0x8
+  u16 move2;               // 0xA
+  u16 move3;               // 0xC
+  u16 move4;               // 0xE
+  u32 trainerRnd;          // 0x10
+  char16 *trainerNamePtr;  // 0x14
+  char16 *nicknamePtr;     // 0x18
+  u32 catchFloorID;        // 0x1C
+  u32 exp;                 // 0x20
+};
 
 #endif
